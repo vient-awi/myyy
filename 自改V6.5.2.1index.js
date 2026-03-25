ad78da6da55c9fe14ee048a2dc89979146fd5af5
vient-awi <517459725@qq.com>
2026-03-26 01:56:45 +0800

    去w

--- 自改V6.5.2.1index.js
+++ 自改V6.5.2.1index.js
@@ -8656,7 +8656,7 @@ a());
                           imageIndex: n,
                           characterName: a.characterName,
                           resourceName: a.resourceName,
-                          wgenderKey: t,
+                          genderKey: t,
                           resultKey: i,
                         });
                   };
